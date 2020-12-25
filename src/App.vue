20e52c22ae0afae6d606d7dba5855d2081e89bf2
dqwdqwdqd <7956918+dqwdqwdqd@user.noreply.gitee.com>
2020-12-25 16:44:38 +0800

    new

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <PhotoWall :listimg="myimg" />
+  </div>
+</template>
+
+<script>
+import PhotoWall from "./components/PhotoWall.vue";
+import axios from 'axios'
+export default {
+  name: "App",
+  components: {
+    PhotoWall,
+  },
+  data() {
+    return {
+      myimg: [],
+    };
+  },
+  created () {
+    // 使用在线mockjs生成的接口：https://www.fastmock.site/mock/4f60d566644d6646e94d679ea86f9287/aixue8/zpq
+    // 使用node+mockjs生成的接口：http://127.0.0.1:3000/getphoto
+      axios.get('https://www.fastmock.site/mock/4f60d566644d6646e94d679ea86f9287/aixue8/zpq')
+      .then(res => {
+        console.log(res.data.list)
+        res.data.list.forEach(item => {
+          this.myimg.push(item.src)
+        })
+      })
+    },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/PhotoWall.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>kw33照片墙项目</h1>
+    <ul class="wall">
+      <li v-for="(item, index) in listimg" :key="index">
+        <img :src="item" alt="" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["listimg"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$color: red;
+.wall {
+  list-style: none;
+  width: 1000px;
+  min-height: 500px;
+  margin: 50px auto;
+  display: flex;
+  flex-wrap: wrap;
+
+  li {
+    width: 230px;
+    margin: 10px 10px;
+
+    img {
+      width: 100%;
+      border: 1px solid $color;
+      padding: 3px;
+      border-radius: 5px;
+    }
+  }
+}
+</style>
